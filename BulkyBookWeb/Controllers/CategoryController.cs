bff26a3be5574d30a842dc2198808ff09c70b458
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-10-26 11:41:47 +0500

    Added Update-Action

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -23,4 +23,48 @@ public class CategoryController:Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {   
+        if(obj.Name==obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name","The DisplayOrder cannot exactly match the Name");
+        } 
+         if(ModelState.IsValid)
+         {
+            _context.Categories.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(obj);
+    }
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id ==0)
+        {
+            return NotFound();
+        }
+        var categoryId = _context.Categories.Find(id);
+        if(categoryId is null ) return NotFound();
+        return View(categoryId);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if(obj.Name==obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name","The DisplayOrder cannot exactly match the Name");
+        } 
+         if(ModelState.IsValid)
+         {
+            _context.Categories.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(obj);
+    }
+
+    
 }
